ba69f57427da9f759febe59b4c512c52d7b6eb8b
rmerzak <rmerzak@student.1337.ma>
2022-06-14 18:03:03 +0100

    push Wallet

---
+++ Contract/Wallet.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.5.12;
+
+contract Wallet {
+
+    event   Deposit(address sender, uint amount, uint balance);
+    event   Withdraw(uint amount, uint balance);
+    event   Transfer(address to, uint amount, uint balance);
+
+    address payable public owner;
+    constructor () public payable{
+        owner = msg.sender;
+    }
+
+    function deposit() public payable {
+        emit Deposit(msg.sender, msg.value, address(this).balance);
+    }
+
+    function notpayable() public {
+
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner,"not the owner");
+        _;
+    }
+
+    function withdraw(uint _amount) public onlyOwner {
+        owner.transfer(_amount);
+        emit Withdraw(_amount, address(this).balance);
+    }
+
+    function transfer(address payable _to, uint _amount) public onlyOwner {
+        _to.transfer(_amount);
+        emit Transfer(_to, _amount, address(this).balance);
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+    
+}
